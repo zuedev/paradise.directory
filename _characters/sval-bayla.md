ae1a62422f0cf95f89d8400f82a937103c37aa5f
zuedev <zuedev@gmail.com>
2020-05-29 04:56:15 +0100

    remove placeholder code for now

--- _characters/sval-bayla.md
+++ _characters/sval-bayla.md
@@ -1,7 +1,5 @@
 ---
 title: Sval Bayla
-libPlaceholder: true
-featuredPlaceholder: true
 thumbnail: https://i.imgur.com/RiW4poe.jpg
 ---
 
--- assets/js/placeholder.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-  if (_state.page.featuredPlaceholder) {
-    document.getElementsByClassName("placeholder");
-  }
-  document.getElementsByClassName("placeholder");
-};
